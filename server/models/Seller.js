9d7d6fea0451771bb76ae074c2d9d4143a72eac1
danialsyed66 <danialsyed66@gmail.com>
2022-03-07 00:12:19 +0500

    Update Seller.js

--- server/models/Seller.js
+++ server/models/Seller.js
@@ -1,26 +1,6 @@
 const mongoose = require('mongoose');
 
-const schema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: [true, 'Seller name is required'],
-    },
-    url: {
-      type: String,
-      required: [true, 'Seller url is required'],
-      default: function () {
-        return `${process.env.CLIENT_URL}seller/${this['_id']}`;
-      },
-    },
-    logo: {
-      url: { type: String, required: true },
-      public_id: String,
-    },
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  },
-  { timestamps: true }
-);
+const schema = new mongoose.Schema();
 
 const Model = mongoose.model('Seller', schema);
 
